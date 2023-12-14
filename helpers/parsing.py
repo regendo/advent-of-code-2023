9752b3ba70e45cbd229652d385b544f06bf7e2f1
Stef Böhm <regendo@users.noreply.github.com>
2023-12-14 17:12:57 +0100

    Yay for 3.10

--- helpers/parsing.py
+++ helpers/parsing.py
@@ -1,10 +1,10 @@
 from io import TextIOWrapper
-from typing import Callable, List, TypeVar, Union
+from typing import Callable, List, TypeVar
 
 T = TypeVar("T")
 
 
-def map_lines(text: Union[str, TextIOWrapper], fn: Callable[[str], T]) -> List[T]:
+def map_lines(text: str | TextIOWrapper, fn: Callable[[str], T]) -> List[T]:
     if isinstance(text, TextIOWrapper):
         return [fn(line) for line in text.readlines()]
     else:
