b6fccbff91105a3bdb3bdc6032d0b12a91486db8
Stef Böhm <regendo@users.noreply.github.com>
2023-12-14 15:26:35 +0100

    Part 2

--- day02/cube_conundrum.py
+++ day02/cube_conundrum.py
@@ -11,6 +11,10 @@ class BallSet:
     green: int
     blue: int
 
+    def power(self) -> int:
+        """Product (multiplication) of the individual colored balls."""
+        return self.red * self.green * self.blue
+
 
 @dataclass
 class Game:
@@ -26,6 +30,15 @@ class Game:
             ):
                 return False
         return True
+
+    def minimum_necessary_balls(self) -> BallSet:
+        necessary = BallSet(0, 0, 0)
+        for reveal in self.reveals:
+            necessary.red = max(necessary.red, reveal.red)
+            necessary.green = max(necessary.green, reveal.green)
+            necessary.blue = max(necessary.blue, reveal.blue)
+
+        return necessary
 
 
 def parse_game(line: str) -> Game:
@@ -55,3 +68,11 @@ def solve_1():
             game.id for game in games if game.is_possible_with_balls(available_balls)
         ]
         print(f"Day 02 Part 1: {sum(possible_games)}")
+
+
+def solve_2():
+    with open("day02/input.txt", "r") as file:
+        games = map_lines(file, parse_game)
+        powers = [game.minimum_necessary_balls().power() for game in games]
+
+        print(f"Day 02 Part 2: {sum(powers)}")
--- solve.py
+++ solve.py
@@ -4,3 +4,4 @@ import day02.cube_conundrum
 day01.trebuchet.solve_1()
 day01.trebuchet.solve_2()
 day02.cube_conundrum.solve_1()
+day02.cube_conundrum.solve_2()
